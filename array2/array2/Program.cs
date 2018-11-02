bc64cdcf889148174cda2490aebba985474d3ba2
tanakorn128 <60030041@kmitl.ac.th>
2018-11-02 15:23:44 +0700

    ok

---
+++ array2/array2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[10];
+            //data[-1] = 0;
+            data[1] = 1;
+            data[9] = 9;
+            //data[10] = 10;
+            // Console.WriteLine(data[-1]);
+            // Console.WriteLine(data[1]);
+            // Console.WriteLine(data[9]);
+            //   Console.WriteLine(data[10]);
+            // Console.WriteLine(data[3]+data[8]);
+            //for (int i = 0; i < 10; i++)
+            //  {
+            //  Console.WriteLine(data[i]);
+            //   }
+            Console.WriteLine(data[1]+data[9]);
+            
+            Console.ReadLine();
+        }
+    }
+}
