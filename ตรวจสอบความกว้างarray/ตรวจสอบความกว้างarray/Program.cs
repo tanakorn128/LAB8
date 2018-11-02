a2eff809d9106169a820e2a2ce8f96a1961364b7
tanakorn128 <60030041@kmitl.ac.th>
2018-11-02 15:25:15 +0700

    ok

---
+++ ตรวจสอบความกว้างarray/ตรวจสอบความกว้างarray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ตรวจสอบความกว้างarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[10];
+            Student[] student = new Student[31];
+            Console.WriteLine(data.Length);
+            Console.WriteLine(student.Length);
+            Console.ReadLine();
+
+        }
+        class Student     // nested class
+        {
+            private string name;
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+        }
+
+    }
+}
